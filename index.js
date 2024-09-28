36f4d8208155855398f30fcbb60b3390cc8f9d67
mariia-ty <tymoshenkom@icloud.com>
2024-09-27 22:50:23 -0400

    done all assignments

---
+++ index.js
@@ -0,0 +1,66 @@
+// Створити масив з трьох чисел. Змінити значення другого елемента масиву на 10.
+
+console.log("Assignment 1");
+let arr = [1, 2, 3];
+
+arr[1] = 20;
+console.log(arr);
+
+// Створити масив із трьох рядків. Додати до масиву ще одну рядків.
+
+// console.log("Assignment 2");
+// let str = ["hello", "hi", "salut"]; 
+
+// str[3] = "bonjour";
+// console.log(str);
+
+// Створити скрипт який поверне суму всіх чисел в масиві.
+
+// console.log("Assignment 3");
+// let sum = [10, 20, 30];
+// let total = 0; 
+
+// for (let i = 0; i < sum.length; i++) 
+//     total += sum[i];
+
+// console.log(total);
+
+// Створити масив з 5-ти чисел. Вивести на екран всі елементи масиву за допомогою циклу for.
+
+// console.log("Assignment 4");
+// let nums = [2, 78, 90, 54, 8];
+
+// for (let n = 0; n < nums.length; n++)
+//     console.log(nums[n]);
+
+// Створити масив із 5-ти рядків. Вивести на екран кожен рядокз масиву, який містить більше 5-ти символів.
+// console.log("Assignment 5");
+// let words = ["plant", "vehicle", "motorcycle", "length", "car"];
+
+// for (let w = 0; w < words.length; w++){
+//     if (words[w].length > 5)
+//         console.log(words[w]);
+//     else
+//         continue;    
+// }
+
+// Створити масив з 10-ти чисел. Знайти та вивести на екран максимальне значення з масиву.
+// console.log("Assignment 6");
+// let num_list = [5, 78, 20, 35, 9, 80, 12, 29, 140, 9]
+// let max_num = 0;
+
+// for (let m = 0; m < num_list.length; m++){
+//     if (num_list[m] > max_num)
+//         max_num = num_list[m];
+// }
+// console.log(max_num);
+
+// Створити масив з 10-ти чисел. Знайти всі парні числа в масиві та вивести їх на екран.
+// console.log("Assignment 7");
+// let list = [28, 17, 18, 36, 11, 74, 3, 33, 44, 13];
+
+// for (let t = 0; t < list.length; t++){
+//     if (list[t] % 2 == 0)
+//         console.log(list[t]);
+        
+// }
